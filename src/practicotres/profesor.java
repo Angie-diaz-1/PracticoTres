e94e810f06772b0078f7f16a5985634ca42fbdd3
Angie-Diaz <diaz.angie.2352@eam.edu.co>
2023-05-04 13:27:59 -0500

    Agregada la clase profesor con sus atributos

---
+++ src/practicotres/profesor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practicotres;
+
+/**
+ *
+ * @author Angie Diaz
+ */
+public class profesor {
+    
+}
